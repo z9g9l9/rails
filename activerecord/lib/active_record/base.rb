480c96e4222ed62ddcfd090e4eb55189536d2bba
Aman Gupta <aman@tmm1.net>
2014-01-08 17:28:14 -0800

    avoid instance_eval if possible

--- activerecord/lib/active_record/base.rb
+++ activerecord/lib/active_record/base.rb
@@ -3072,7 +3072,11 @@ module ActiveRecord #:nodoc:
       # Interpolate custom SQL string in instance context.
       # Optional record argument is meant for custom insert_sql.
       def interpolate_sql(sql, record = nil)
-        instance_eval("%@#{sql.gsub('@', '\@')}@")
+        if sql =~ /\#\{/
+          instance_eval("%@#{sql.gsub('@', '\@')}@")
+        else
+          sql
+        end
       end
 
       # Initializes the attributes array with keys matching the columns from the linked table and
